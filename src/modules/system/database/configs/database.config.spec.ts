ff465bef832928ed1f24c64a7206361ed37b4e36
Natanael1234 <{ID}+{username}@users.noreply.github.com>
2023-03-31 21:22:30 -0300

    jwt configs tests

--- src/modules/system/database/configs/database.config.spec.ts
+++ src/modules/system/database/configs/database.config.spec.ts
@@ -2,33 +2,33 @@ import { envTestParameters } from '../../../../.jest/env-test-parameters';
 import { DatabaseConfigs } from './database.config';
 
 describe('DatabaseConfig', () => {
-  it('EncryptationConfigs should be defined', () => {
+  it('DB should be defined', () => {
     expect(DatabaseConfigs).toBeDefined();
   });
 
-  it('DB type should be defined', () => {
+  it('type should be defined', () => {
     expect(DatabaseConfigs.DB_TYPE).toEqual(envTestParameters.DB_TYPE);
   });
 
-  it('DB host should be defined', () => {
+  it('host should be defined', () => {
     expect(DatabaseConfigs.DB_HOST).toEqual(envTestParameters.DB_HOST);
   });
 
-  it('DB port should be defined', () => {
+  it('port should be defined', () => {
     expect(DatabaseConfigs.DB_PORT).toEqual(+envTestParameters.DB_PORT);
   });
 
-  it('DB name should be defined', () => {
+  it('name should be defined', () => {
     expect(DatabaseConfigs.DB_DATABASE_NAME).toEqual(
       envTestParameters.DB_DATABASE_NAME,
     );
   });
 
-  it('DB username should be defined', () => {
+  it('username should be defined', () => {
     expect(DatabaseConfigs.DB_USERNAME).toEqual(envTestParameters.DB_USERNAME);
   });
 
-  it('DB password should be defined', () => {
+  it('password should be defined', () => {
     expect(DatabaseConfigs.DB_PASSWORD).toEqual(envTestParameters.DB_PASSWORD);
   });
 });
