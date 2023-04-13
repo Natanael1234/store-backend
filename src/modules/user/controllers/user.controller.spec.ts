a24014d55238ca38cb4882fae32bbb30c504e5a5
Natanael1234 <{ID}+{username}@users.noreply.github.com>
2023-04-13 16:55:32 -0300

    user controller test improvements

--- src/modules/user/controllers/user.controller.spec.ts
+++ src/modules/user/controllers/user.controller.spec.ts
@@ -4,12 +4,6 @@ import { UserController } from './user.controller';
 
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import {
-  testCreateUser,
-  testFindUserForId,
-  testFindUsers,
-  testUpdateUser,
-} from '../../../test/test-user-utils';
 import { UserEntity } from '../models/user/user.entity';
 
 describe('UserController', () => {
@@ -29,33 +23,7 @@ describe('UserController', () => {
     await module.close(); // TODO: é necessário?
   });
 
-  describe('insert', () => {
-    it('should create users', async () => {
-      await testCreateUser(userRepo, (userData: any) =>
-        userController.create(userData),
-      );
-    });
-  });
-
-  describe('update', () => {
-    it('should update user', async () => {
-      await testUpdateUser(userRepo, (userId: number, userData: any) =>
-        userController.update({ userId }, userData),
-      );
-    });
-  });
-
-  describe('find all', () => {
-    it('should all users', async () => {
-      await testFindUsers(userRepo, () => userController.findAll());
-    });
-  });
-
-  describe('find one', () => {
-    it('should find one user', async () => {
-      await testFindUserForId(userRepo, (userId: number) =>
-        userController.findForId({ userId }),
-      );
-    });
+  it('sould be defined', async () => {
+    expect(userController).toBeDefined();
   });
 });
